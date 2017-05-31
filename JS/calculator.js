9c4d727ca9e4828da4a8301155080c0c19e993d0
piedyl <piedyl@gmail.com>
2017-05-31 02:11:24 -0400

    Verified Version 1.0

--- JS/calculator.js
+++ JS/calculator.js
@@ -12,8 +12,32 @@ function calculator(){
 		calc:null
 	};
 	result = input(result);
-	var sum = result.num1 + result.num2;
-	alert('type of num1 ='+typeof(result.num1));
-	alert('num1='+result.num1);
-	alert('sum='+sum);
+	var sum;
+	switch(result.calc){
+		case "+":
+			sum = result.num1 + result.num2;
+			break;
+		case "-":
+			sum = result.num1 - result.num2;
+			break;
+		case "*":
+			sum = result.num1 * result.num2;
+			break;
+		case "/":
+			if(result.num2 == 0){
+				alert('分母数字不能为0');
+				break;
+			}
+			else{
+				sum = result.num1 / result.num2;
+				break;
+			}
+			
+		}
+	if(typeof(sum)=="undefined"){
+		alert('请重新输入数字');
+	}
+	else{
+		alert('计算结果='+sum);		
+	}
 }
